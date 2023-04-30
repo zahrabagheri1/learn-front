6ad5ec1750a26ef21109eb55c675cc36aa79ec70
zahrabagheri1 <zahrabagheriuk@gmail.com>
2023-04-30 13:03:20 +0430

    jsx component

---
+++ myfirstreact/src/Car.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Car extends React.Component{
+    render(){
+        return(
+            <h1>GoodBye</h1>
+        )
+    }
+}
+
+export default Car;
---
+++ myfirstreact/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+let str = "Do you?!"
+//* With JSX you can write expressions inside curly braces { }.
+let myElement0 = <h1>I Love JSX! {str}</h1>
+
+//* To write HTML on multiple lines, put the HTML inside parentheses ()
+const myElement1 = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
+
+//* The HTML code must be wrapped in ONE top level element.
+const myElement2 = (
+  <div>
+    <p>I am a paragraph.</p>
+    <p>I am a paragraph too.</p>
+  </div>
+);
+
+// <React.Fragment> </React.Fragment>
+const myElement3 = (
+  <React.Fragment>
+    <p>I am a paragraph.</p>
+    <p>I am a paragraph too.</p>
+  </React.Fragment>
+);
+
+ReactDOM.render( <App name="ali"  color= "bluecd"/>, 
+ document.getElementById('root')
+
+);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+    
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
+
+
+
