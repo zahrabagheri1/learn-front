b12d651dc5c91b2c8aeb057794bcc32b9071e836
zahrabagheri1 <zahrabagheriuk@gmail.com>
2023-07-11 01:53:39 +0330

    update

---
+++ JavaScript/arrow-function.js
@@ -0,0 +1,31 @@
+
+//todo: normal function syntax
+// function add(num1, num2) {
+//   console.log( num1 + num2)
+// }
+
+//todo: arrow function syntax
+// const add = (num1, num2 )=>{
+//   console.log( num1 + num2)
+// }
+
+// let add = (num1, num2) =>  console.log( num1 + num2);
+
+// add(3,3)
+
+
+//todo: normal functin has arguments object
+// function print() {
+//   console.log(arguments)
+//   console.log(arguments[0])
+//   console.log(arguments[2])
+// }
+// print("Zahra", 22, true)
+
+
+//todo: arrow function  does n't exist in it
+
+const print = () => console.log(arguments[1])
+
+print("zahra", 500 , false)
+
