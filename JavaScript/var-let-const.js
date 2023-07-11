c25d8ae5e481ecbf8376d975e985f71a87bff170
zahrabagheri1 <zahrabagheriuk@gmail.com>
2023-07-11 10:57:08 +0330

    var

--- JavaScript/var-let-const.js
+++ JavaScript/var-let-const.js
@@ -1,4 +1,13 @@
 //? Difference between var let const
 
+// console.log(number)
+///* number is not defined
+
+console.log(number);
+var number = 10 ;
+///* undefined
 
 
+var number = 10 ;
+// console.log(number)
+///* 10
